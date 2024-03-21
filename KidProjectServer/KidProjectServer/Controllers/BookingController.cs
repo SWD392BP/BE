4504d91e4e67b16af30a1f99595571c088cb9393
Pham Phương Nam <namdeptrai13579@gmail.com>
2024-03-21 21:21:05 +0700

    Fix

--- KidProjectServer/KidProjectServer/Controllers/BookingController.cs
+++ KidProjectServer/KidProjectServer/Controllers/BookingController.cs
@@ -36,6 +36,10 @@ namespace KidProjectServer.Controllers
         public async Task<ActionResult<IEnumerable<Booking>>> GetByID(int id)
         {
             Booking booking = await _bookingService.GetBookingByID(id);
+            if(booking == null)
+            {
+                return Ok(ResponseHandle<Booking>.Error("Booking not found"));
+            }
             return Ok(ResponseHandle<Booking>.Success(booking));
         }
 
